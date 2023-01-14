cc3855a2b5e988386ea27ce1a4b7bbfd8cdacc94
davidhsc <davidhsc@gmail.com>
2023-01-14 00:45:55 -0600

    agregando funcionalidad tooltip

--- js/app.js
+++ js/app.js
@@ -2,6 +2,14 @@
 const body = d3.select("body"); //selector principal body
 let data = null;                //variable principal de datos
 
+// objetos para el tooltip
+const tooltip = body.select("#tooltip");
+const tooltipnombrebuque = body.select("#tooltipnombrebuque");
+const tooltipnaviera = body.select("#tooltipnaviera");
+const tooltipmaniobrista = body.select("#tooltipmaniobrista");
+const tooltipeslora = body.select("#tooltipeslora");
+const tooltipcalado = body.select("#tooltipcalado");
+
 // DEFINICIÓN DE LA FUNCIÓN PRICIPAL
 // llamará a las demás funciones después de cargar los datos
 const loadDataAndRenderAll = async() => {
@@ -47,8 +55,8 @@ const renderDimensionesBuques = (data)=> {
     y.domain([0, d3.max(data, (d) => +d.CALADO * 1.1)])
     
     // construir el svg
-    const dimensionestab =body.select("#dimensionestab-pane");
-    const svgdimensionesbuques = dimensionestab.append("svg")
+    const graf =body.select("#graf");
+    const svgdimensionesbuques = graf.append("svg")
     .attr("width",anchototal)
     .attr("height", altototal)
     .attr("class", "fig");
@@ -73,7 +81,18 @@ const renderDimensionesBuques = (data)=> {
         .attr('font-family', 'FontAwesome')
         .attr("class", d=> "buque " + d.MANIOBRISTA.substring(0,3).toLowerCase() )
         .text(function(d) { return '\uf21A' })
-         ;
+        //ampliar el icono del buque al seleccionarlo (antes quita la clase seleccionado de los anteriores)
+        .on("mouseover", function(e,d) {
+            // quitar la selección anterior 
+            console.log(d);
+            d3.selectAll(".seleccionado").classed("seleccionado",false);
+            // seleccionar el buque (ampliar el icono mediante css)
+            d3.select(this).classed("seleccionado", !d3.select(this).classed("seleccionado"))
+            showTooltip(d);
+          })
+        .on("mouseout", function(e,d) {
+            hideTooltip();
+        })
         /*
         .append('circle')
             .attr('cx', d=> x(+d.ESLORA) )
@@ -96,7 +115,7 @@ const renderDimensionesBuques = (data)=> {
         .attr("x", anchototal/2)
         .attr("y", altototal - 30)
         .attr("text-anchor", "middle")
-        .text("Eslora de los buques");
+        .text("Eslora de los buques (metros)");
     //   título del eje y (con rotación)
     g.append("g")
         .attr("transform",`translate(0,${altototal/2})`)
@@ -104,9 +123,26 @@ const renderDimensionesBuques = (data)=> {
         .attr("y",-35)
         .attr("transform","rotate(-90)")
         .attr("text-anchor", "middle")
-        .text("Calado de los buques");
-        
+        .text("Calado de los buques (metros)");
+
+    //definición de la función auxiliar showTooltip
+    const showTooltip = (d)=>{
+        tooltip.style("display","block");
+        tooltip.style("left", x(+d.ESLORA)+10+"px").style("top", y(+d.CALADO)+20+"px")
+        tooltipnombrebuque.text(d.NOM_BUQUE);
+        tooltipnaviera.text(d.NOM_NAVIERA);
+        tooltipmaniobrista.text(d.MANIOBRISTA);
+        tooltipeslora.text(d.ESLORA + " metros");
+        tooltipcalado.text(d.CALADO + " metros");
+    }
+    //definición de la función auxiliar hideTooltip
+    const hideTooltip = (d)=>{
+        tooltip.style("display","none");
+    }
+
 }
+
+
 
 //EJECUTAR LA FUNCIÓN PRINCIPAL
 loadDataAndRenderAll()
